1b604728210cdcd71891c0aac813868b71475e93
DeepakAd1 <deepakad212@gmail.com>
2024-04-21 22:23:19 +0530

    palin changes

--- gfg.java
+++ gfg.java
@@ -20,6 +20,7 @@ class GFG {
 	{
 		int n = 123464321;
 		int reverseN = reversNumber(n);
+		System.out.println("Hello");
 		System.out.println("Reverse of n = " + reverseN);
 
 		// Checking if n is same
